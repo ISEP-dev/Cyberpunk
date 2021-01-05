0581fc003aa4c8aa52e282bd867d4b5730e8f6ab
bastiengrignon <basgrignon@gmail.com>
2021-01-05 16:34:39 +0100

    ADD test

---
+++ cyberpunk/src/__tests__/App.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import Merc from '../page/merc';
+
+test('renders learn react link', () => {
+  /*render(<Merc />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();*/
+});
