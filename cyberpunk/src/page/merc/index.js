b4a54336b9c30295c75da66f35306d791aac0da0
bastiengrignon <basgrignon@gmail.com>
2021-01-06 09:11:08 +0100

    ADD Modal component example

--- cyberpunk/src/page/merc/index.js
+++ cyberpunk/src/page/merc/index.js
@@ -1,7 +1,15 @@
+import Modal from "../../component/Modal";
+
 const Merc = () => {
+
+    const submit = () => {
+        console.log("On modal submit example")
+    }
     return (
         <div className="flex justify-center">
             Merc page
+            <Modal title="Add a new merc" okButton="Create"
+                   description={"It can be a text or HTML"} onSubmit={submit}/>
         </div>
     );
 };
