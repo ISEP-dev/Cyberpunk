56ebcb740136cea41a46919cd1fc74196e8516b3
bastiengrignon <basgrignon@gmail.com>
2021-01-08 14:20:05 +0100

    FIX svg color

--- cyberpunk/src/component/Modal/index.js
+++ cyberpunk/src/component/Modal/index.js
@@ -19,10 +19,10 @@ const Modal = ({title, description, icon, cancelButton, okButton, visibility, on
                     role="dialog" aria-modal="true" aria-labelledby="modal-headline">
                     <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                         <span className="absolute top-0 right-0 text-2xl cursor-pointer pt-1 pr-4"
-                              onClick={() => setIsVisible(false)}>&times;</span>
+                              title="Close" onClick={() => setIsVisible(false)}>&times;</span>
                         <div className="sm:flex sm:items-start">
                             <div
-                                className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                                className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-blue-100 sm:mx-0 sm:h-10 sm:w-10">
                                 {icon}
                             </div>
                             <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
@@ -53,7 +53,7 @@ const Modal = ({title, description, icon, cancelButton, okButton, visibility, on
 }
 
 Modal.defaultProps = {
-    icon: <svg className="h-6 w-6 text-red-600" xmlns="http://www.w3.org/2000/svg" fill="none"
+    icon: <svg className="h-6 w-6 text-blue-600" xmlns="http://www.w3.org/2000/svg" fill="none"
                viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
               d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"/>
