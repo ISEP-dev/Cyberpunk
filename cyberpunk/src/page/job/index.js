d5238843fee7dff154fd4b40b4db3185871a56fd
Mautrand <mathilde.autrand@outlook.fr>
2021-01-12 14:06:00 +0100

    first try

--- cyberpunk/src/page/job/index.js
+++ cyberpunk/src/page/job/index.js
@@ -1,10 +1,30 @@
-const Job = () => {
+const Jobs = (props) => {
 
+    const jobs = props.jobs
+    console.log(jobs)
+
+    const jobList = weapons.map(element =>{
+        return(
+            <div key={element.id}>
+                <div>
+                    <div><h1>{element.title}</h1></div>
+                </div>
+                <div className='flex'>
+                    <div className='w-1/4'>
+                        <p>Fixer :{element.fixer}</p>
+                        <p>Henchmen :{element.henchmenCount}</p>
+                        <p>reward :{element.reward}</p>
+                    </div>
+                    <p className='w-3/4'>{element.description}</p>
+                </div>
+            </div>
+        )
+    })  
     return (
-        <div className="flex">
-            Job page
+        <div>
+            {jobList}
         </div>
-    )
-}
+    );
+};
 
-export default Job;
+export default Jobs;
