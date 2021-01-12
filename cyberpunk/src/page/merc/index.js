8ddb5b657caddcdee1f7b050523e29361d28409a
Perrault <lucas.perrault@hotmail.com>
2021-01-12 14:00:12 +0100

    FIX after merge

--- cyberpunk/src/page/merc/index.js
+++ cyberpunk/src/page/merc/index.js
@@ -1,32 +1,16 @@
-import Modal from "../../component/Modal";
 import {getAllMercsAsync} from "../../service/merc";
 import {useEffect} from "react";
 
 const Merc = () => {
-    const [weapons, setWeapons] = useState([])
-    useEffect(() => {
-        getAllWeapons().then((res) => {
-            let weapon = res.data
-            console.log(weapon)
-            setWeapons(weapon)
-
     useEffect(() => {
         getAllMercsAsync().then((res) => {
             console.log(res)
         })
     }, [])
 
-    const submit = () => {
-        console.log("On modal submit example")
-    }
     return (
-        // <div className="flex justify-center">
-        //     Merc page
-        //     <Modal title="Add a new merc" okButton="Create"
-        //            description={"It can be a text or HTML"} onSubmit={submit}/>
-        // </div>
         <div>
-            <Weapons weapons = {weapons}/>
+            Merc page
         </div>
     );
 };
