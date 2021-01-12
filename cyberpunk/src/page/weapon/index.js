5e01bb8e336a86d0ffed14949b325d6108e1cacc
Perrault <lucas.perrault@hotmail.com>
2021-01-12 14:00:12 +0100

    FIX after merge

--- cyberpunk/src/page/weapon/index.js
+++ cyberpunk/src/page/weapon/index.js
@@ -1,12 +1,11 @@
-import Modal from "../../component/Modal";
 import {useEffect, useState} from "react";
 import Weapons from "../../component/Modal/Weapons";
-import {getAllWeapons} from "../../service/weapon";
+import {getAllWeaponsAsync} from "../../service/weapon";
 
 const Weapon = () => {
     const [weapons, setWeapons] = useState([])
     useEffect(() => {
-        getAllWeapons().then((res) => {
+        getAllWeaponsAsync().then((res) => {
             let weapon = res.data
             console.log(weapon)
             setWeapons(weapon)
