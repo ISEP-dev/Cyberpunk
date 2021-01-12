7f40ce5f66a2b26a9a13828cad413f361903e1da
Mautrand <mathilde.autrand@outlook.fr>
2021-01-12 14:06:00 +0100

    first try

--- cyberpunk/src/page/job/index.js
+++ cyberpunk/src/page/job/index.js
@@ -1,14 +1,34 @@
 import {useEffect} from "react";
 import {redirectToAuthPageIfNotConnected} from "../../service/local-auth";
 
-const Job = () => {
+const Job = (props) => {
     useEffect(() => redirectToAuthPageIfNotConnected(), [])
 
-    return (
-        <div className="flex">
-            Job page
-        </div>
-    )
-}
+    const jobs = props.jobs
+    console.log(jobs)
 
-export default Job;
+    const jobList = weapons.map(element =>{
+        return(
+            <div key={element.id}>
+                <div>
+                    <div><h1>{element.title}</h1></div>
+                </div>
+                <div className='flex'>
+                    <div className='w-1/4'>
+                        <p>Fixer :{element.fixer}</p>
+                        <p>Henchmen :{element.henchmenCount}</p>
+                        <p>reward :{element.reward}</p>
+                    </div>
+                    <p className='w-3/4'>{element.description}</p>
+                </div>
+            </div>
+        )
+    })  
+    return (
+        <div>
+            {jobList}
+        </div>
+    );
+};
+
+export default Jobs;
