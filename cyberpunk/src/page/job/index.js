8942b35685e06b2dc82cfe785979b298419a883b
Mautrand <mathilde.autrand@outlook.fr>
2021-01-12 15:06:03 +0100

    affichage ok

--- cyberpunk/src/page/job/index.js
+++ cyberpunk/src/page/job/index.js
@@ -1,14 +1,25 @@
+import { useEffect, useState } from "react";
+import { getAllJobs } from "../../service/job"
 import JobCard from "../../component/JobCard";
 
 const Jobs = (props) => {
 
-    const jobs = props.jobs
-    console.log(jobs)
+    // const jobs = props.jobs
+    // console.log(jobs)
 
-    const jobList = jobs.map(element =>{ return(<JobCard />)})
+    const [jobs, setjobs] = useState([])
+
+    useEffect(() => {
+        getAllJobs().then((res) => {
+            let job = res.data
+            console.log(job)
+            setjobs(job)
+        })
+    })
+
     return (
-        <div>
-            {jobList}
+        <div className="flex flex-wrap w-full">
+            {jobs.map(element => { return(<JobCard element={element}/>)})}
         </div>
     );
 };
