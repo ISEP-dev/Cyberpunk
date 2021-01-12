11628c84b7048b890545b20e6a42bf562fd7b1d3
Mautrand <mathilde.autrand@outlook.fr>
2021-01-12 14:49:19 +0100

    Bonne structure

--- cyberpunk/src/page/job/index.js
+++ cyberpunk/src/page/job/index.js
@@ -1,25 +1,11 @@
+import JobCard from "../../component/JobCard";
+
 const Jobs = (props) => {
 
     const jobs = props.jobs
     console.log(jobs)
 
-    const jobList = jobs.map(element =>{
-        return(
-            <div key={element.id}>
-                <div>
-                    <div><h1>{element.title}</h1></div>
-                </div>
-                <div className='flex'>
-                    <div className='w-1/4'>
-                        <p>Fixer :{element.fixer}</p>
-                        <p>Henchmen :{element.henchmenCount}</p>
-                        <p>reward :{element.reward}</p>
-                    </div>
-                    <p className='w-3/4'>{element.description}</p>
-                </div>
-            </div>
-        )
-    })  
+    const jobList = jobs.map(element =>{ return(<JobCard />)})
     return (
         <div>
             {jobList}
