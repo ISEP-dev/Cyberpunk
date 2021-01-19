3550511da6bc35eb475af6c935b1c9cce839fdfc
bastiengrignon <basgrignon@gmail.com>
2021-01-19 18:51:01 +0100

    FIX ProgressBar for firerate stat

--- cyberpunk/src/component/PorgressBar/index.js
+++ cyberpunk/src/component/PorgressBar/index.js
@@ -3,7 +3,7 @@ const ProgressBar = ({number, color}) => (
         <div className="absolute mt-1.5 bg-gray-600 rounded-full w-full h-4"/>
         <div className={`absolute mt-1.5 bg-${color}-600 rounded-full w-1/2 h-4`}
              style={{width: `${number}%`}}/>
-        <div className="absolute mt-1.5 w-1/2 text-white text-xs text-center">{number}</div>
+        <div className="absolute mt-1.5 w-1/2 text-white text-xs text-center">{color === "green" ? number / 10 : number}</div>
     </div>
 )
 
