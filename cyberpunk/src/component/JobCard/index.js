20795ff4cb84f526c9c91f126ccc703f389389fb
Perrault <lucas.perrault@hotmail.com>
2021-01-19 23:13:02 +0100

    MOD job card

--- cyberpunk/src/component/JobCard/index.js
+++ cyberpunk/src/component/JobCard/index.js
@@ -1,50 +1,61 @@
-import { getWeaponByIdAsync } from "../../service/weapon"
-import {completeJobAsync} from "../../service/job";
+import { useState } from 'react';
+import PropTypes from "prop-types";
+import MercsSelection from "../MercsSelection";
+import JobFightDialog from "../JobFightDialog";
 
-const fakeMerc = {
-    id: 3,
-    weaponId: 1
-}
 
-const JobCard = ({job}) => {
-    const fight = (job, merc) => {
-        const weapon = getWeaponByIdAsync(merc.weaponId)
-        let mercsLife = 100
-        for(let k=0; k<job.henchmenCount;k++){
-            let henchmenLife = 100
-            while(henchmenLife>0){
-                for(let i=0; i<weapon.firerate; i++){
-                    if(Math.floor(Math.random() * 100) < weapon.accuracy){
-                        henchmenLife -= weapon.power
-                    } 
-                } if(henchmenLife>0){
-                    mercsLife-=10
-                } if(mercsLife<=0){
-                    // the mercs is killed
-                    // Break
-                }
-            }
+const JobCard = ({job, mercs}) => {
+    const [mercSelected, setMercSelected] = useState();
+    const [isFightDialogVisible, setIsFightDialogVisible] = useState(false);
+
+    const fight = () => {
+        if (!mercSelected && job.isAvailable) {
+            alert("You need to select a merc before launch the job !");
+            return;
         }
-        completeJobAsync(job.id, merc.id).then(res => console.log(res))
+
+        setIsFightDialogVisible(true);
     }
 
     return (
-        <div key={job.id} style={{minWidth: '25rem', width: '48%'}} className="rounded-md bg-gray-800 p-5 text-white grid justify-items-stretch space-y-4 mb-4">
+        <div style={{minWidth: '25rem', width: '48%'}}
+             className={`${!job.isAvailable ? "bg-gray-400 opacity-90" : "bg-gray-800"} rounded-md p-5 text-white grid justify-items-stretch mb-4`}>
             <h1 className="text-center font-bold text-yellow-400 text-2xl">{job.title}</h1>
             <div className="flex flex-row">
                 <div className='w-3/5'>
                     <p className="break-words text-justify">{job.description}</p>
                 </div>
                 <div className='w-2/5 ml-4 flex flex-col justify-between'>
-                    <p className="mb-1">Fixer: job.fixer}</p>
+                    <p className="mb-1">Fixer: {job.fixer}</p>
                     <p className="mb-1">Henchmen: {job.henchmenCount}</p>
                     <p className="mb-3">Reward: €${job.reward}</p>
-                    <button className="bg-yellow-400 text-gray-900 h-10 rounded-sm w-full cursor-pointer hover:bg-yellow-300"
+                    <MercsSelection mercs={mercs} onSelectMerc={m => setMercSelected(m)} mercSelected={mercSelected}/>
+                    <button className={`${job.isAvailable 
+                                ? "cursor-pointer hover:bg-yellow-300 bg-yellow-400 text-gray-900" 
+                                : "bg-gray-500 text-gray-700"} h-10 rounded-sm w-full mt-2`
+                            }
+                            disabled={!job.isAvailable}
                             type="button"
-                            onClick={() => fight(job, fakeMerc)}>Launch</button>
+                            onClick={fight}><i className="fas fa-play mr-2"/>
+                            Launch
+                    </button>
                 </div>
             </div>
+
+            {
+                !!mercSelected && <JobFightDialog visibility={isFightDialogVisible}
+                                    merc={mercSelected}
+                                    weapon={mercSelected.weapon}
+                                    job={job}
+                                    onClose={() => setIsFightDialogVisible(false)}/>
+            }
         </div>
     );
 }
+
+JobCard.propTypes = {
+    job: PropTypes.object.isRequired,
+    mercs: PropTypes.array.isRequired,
+}
+
 export default JobCard;
