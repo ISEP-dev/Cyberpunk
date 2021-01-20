a91375998f3585314f7c8980b3fd99bc03b778c4
Perrault <lucas.perrault@hotmail.com>
2021-01-20 01:35:18 +0100

    MOD alert comp

--- cyberpunk/src/component/Alert/index.js
+++ cyberpunk/src/component/Alert/index.js
@@ -1,32 +1,27 @@
 import {useEffect, useState} from "react";
 import PropTypes from "prop-types";
+import {NotificationType} from "../../model/notification";
 
-const AlertType = {
-    success: "success",
-    error: "error",
-    warning: "warning",
-    info: "info"
-}
-
-const Alert = ({text, type, timeout, visibility, onClose}) => {
+const Alert = ({text, type, timeout}) => {
+    const [visibility, setVisibility] = useState(true);
     const [color, setColor] = useState("")
     const [icon, setIcon] = useState("")
 
     useEffect(() => {
-        if (type === AlertType.success) {
+        if (type === NotificationType.success) {
             setColor("green")
             setIcon("check-circle")
-        } else if (type === AlertType.error) {
+        } else if (type === NotificationType.error) {
             setColor("red")
             setIcon("exclamation-circle")
-        } else if (type === AlertType.warning) {
+        } else if (type === NotificationType.warning) {
             setColor("yellow")
             setIcon("exclamation-triangle")
-        } else if (type === AlertType.info) {
+        } else if (type === NotificationType.info) {
             setColor("blue")
             setIcon("info-circle")
         }
-        const timer = setTimeout(onClose, timeout)
+        const timer = setTimeout(() => setVisibility(false), timeout)
 
         return () => {
             clearTimeout(timer)
@@ -35,25 +30,22 @@ const Alert = ({text, type, timeout, visibility, onClose}) => {
 
     return (
         <div
-            className={`fixed top-5 left-0 right-0 mx-auto bg-${color}-200 px-6 py-4 my-4 rounded-md text-lg flex items-center justify-center w-1/4 z-20 ${visibility ? "block" : "hidden"} `}>
+            className={`mx-auto bg-${color}-200 px-6 py-4 my-4 rounded-md text-lg flex items-center justify-center w-1/2 ${visibility ? "block" : "hidden"} `}>
             <i className={`fas fa-${icon} text-${color}-800 mr-2`}/>
             <span className={`text-${color}-800`}>{text}</span>
+            <i className="fas fa-times ml-2 cursor-pointer hover:text-gray-700 ml-auto" onClick={() => setVisibility(false)}/>
         </div>
     )
 }
 
 Alert.defaultProps = {
     type: "success",
-    timeout: 5000,
-    visibility: false
 }
 
 Alert.propTypes = {
     text: PropTypes.string.isRequired,
-    type: PropTypes.PropTypes.oneOf([AlertType.success, AlertType.error, AlertType.warning, AlertType.info]).isRequired,
+    type: PropTypes.PropTypes.oneOf([NotificationType.success, NotificationType.error, NotificationType.warning, NotificationType.info]).isRequired,
     timeout: PropTypes.number.isRequired,
-    visibility: PropTypes.bool.isRequired,
-    onClose: PropTypes.func.isRequired
 }
 
 export default Alert
